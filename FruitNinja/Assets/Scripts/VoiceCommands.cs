b15bb9ee78a60af9c74716cd5298ab36449d50b7
Anthony Moore <68195709+antleemoore@users.noreply.github.com>
2024-02-29 13:18:09 -0500

    fruit god mode

--- FruitNinja/Assets/Scripts/VoiceCommands.cs
+++ FruitNinja/Assets/Scripts/VoiceCommands.cs
@@ -1,10 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Windows.Speech;
 
 public class VoiceCommands : MonoBehaviour
 {
     private KeywordRecognizer _keywordRecognizer;
-
+    public Material moldyMaterial;
     private void Start()
     {
         _keywordRecognizer = new KeywordRecognizer(new string[] { "fruit gods" });
@@ -12,12 +14,32 @@ public class VoiceCommands : MonoBehaviour
         _keywordRecognizer.Start();
     }
 
-    private static void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
         Debug.Log("Voice command: " + args.text);
 
         if (args.text == "fruit gods")
         {
+            enemy_movement[] enemies = FindObjectsByType<enemy_movement>(FindObjectsSortMode.None);
+            foreach (enemy_movement enemy in enemies)
+            {
+                // Check if the enemy contains the enemy_movement script
+                if (enemy.GetComponent<enemy_movement>() != null)
+                {
+                    // Access the renderer and change the material
+                    Renderer renderer = enemy.GetComponent<Renderer>();
+                    if (renderer != null)
+                    {
+                        renderer.material = moldyMaterial;
+                        enemy.speed = 0f;
+                        Seed_Shooter[] seedShooters = FindObjectsByType<Seed_Shooter>(FindObjectsSortMode.None);
+                        foreach (Seed_Shooter seedShooter in seedShooters)
+                        {
+                            seedShooter.rate_of_fire = 100;
+                        }
+                    }
+                }
+            }
             Debug.Log("Successful");
         }
     }
