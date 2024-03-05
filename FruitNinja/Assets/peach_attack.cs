4cada6e818b0a6abba5eb519607aa5708847b5c3
FriedRiceBurner <Jose.valentin.serajosef@gmail.com>
2024-03-05 16:20:50 -0500

    Offloading from Personal

---
+++ FruitNinja/Assets/peach_attack.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class peach_attack : MonoBehaviour
+{
+    public GameObject Particles;
+    public AudioSource Sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Fruits")
+        {
+            Debug.Log("Hit fruit");
+            //yield return new WaitForSeconds(2);
+            Instantiate(Particles, col.gameObject.transform.position, Quaternion.identity); ;
+            Destroy(col.gameObject);
+            Sound = GetComponent<AudioSource>();
+            Sound.Play(0);
+            Destroy(gameObject);
+        }
+
+
+    }
+}
